74175f63f12041d95648b156a273962a2e3dac1c
kazirafi71 <kazirafi668@gmail.com>
2022-05-05 20:47:18 +0600

    leet

--- LeetCode/581. Shortest Unsorted Continuous Subarray.py
+++ LeetCode/581. Shortest Unsorted Continuous Subarray.py
@@ -4,20 +4,19 @@ class Solution(object):
 
         ans = 0
         r = []
-        print(res)
+
         for i in range(len(res)):
             if nums[i] != res[i]:
                 r.append(i)
-        
+        print(r)
         if not len(r):
             return 0
         if len(r) == 1:
             return 1
 
-        print(r)
         return r[-1]-r[0]+1
 
 
 ob1 = Solution()
 
-print(ob1.findUnsortedSubarray([2, 6, 4, 8, 10, 9, 15]))
+print(ob1.findUnsortedSubarray([2, 1]))
